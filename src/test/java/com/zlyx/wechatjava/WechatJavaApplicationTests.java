ddabc2831b5213ce5e60b22efe1c431e1ad4c8fd
yinyuanyuan <56661619+zhonglianyinxin@users.noreply.github.com>
2019-10-17 11:04:36 +0800

    项目首次提交

---
+++ src/test/java/com/zlyx/wechatjava/WechatJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlyx.wechatjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
